3cd35a504d737eaa65055ed489e5e2ee13480807
disisid17 <77601095+disisid17@users.noreply.github.com>
2024-02-25 16:45:34 -0500

    change

--- bardpygame.py
+++ bardpygame.py
@@ -10,6 +10,7 @@ import random
 import time
 
 
+
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 pose = mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5)
